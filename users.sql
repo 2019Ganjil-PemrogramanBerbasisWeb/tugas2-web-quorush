18a66e046596f86d03d52886b02bd39c753d85b4
Bhaskaraa <49366063+Bhaskaraa@users.noreply.github.com>
2019-10-21 10:53:06 +0700

    Update SQL
    Mengubah password yang awalnya plaintext menjadi enkripsi

--- users.sql
+++ users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Okt 2019 pada 12.10
--- Versi server: 10.1.35-MariaDB
--- Versi PHP: 7.2.9
+-- Generation Time: Oct 21, 2019 at 05:48 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -35,21 +35,21 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`fullname`, `username`, `password`) VALUES
-('aseeem', 'asu', '1234'),
-('kokoko', 'kulo', '000000'),
-('dddddd', 'kulokuko', '987654'),
-('asuu', 'read', '123456');
+('aseeem', 'asu', '81dc9bdb52d04dc20036dbd8313ed055'),
+('kokoko', 'kulo', '670b14728ad9902aecba32e22fa4f6bd'),
+('dddddd', 'kulokuko', '6c44e5cd17f0019c64b042e4a745412a'),
+('asuu', 'read', 'e10adc3949ba59abbe56e057f20f883e');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`),
