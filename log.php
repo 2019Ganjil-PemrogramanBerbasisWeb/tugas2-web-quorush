413bbe280141d42c916bde930631d6c2556bd0db
anissaidatur <50788092+anissaidatur@users.noreply.github.com>
2019-10-22 18:17:44 +0700

    Update log.php

--- log.php
+++ log.php
@@ -1,30 +1,32 @@
 <?php
 //akitfkan session php
-
+error_reporting(0);
 //hubungkan dengan koneksi
 include 'connect.php';
 
 //mengambil data di form
 $username = $_POST['username'];
-$password = $_POST['password'];
+$password = md5($_POST['password']);
 
 //seleksi dengan data yang sesuai
-$data = mysqli_query($link, " SELECT * FROM datas WHERE name='$username' AND password='$password'");
+$data = mysqli_query($conn, "SELECT * FROM datas WHERE username='$username' AND password='$password'");
 
 //hitung data
 $cek = mysqli_num_rows($data);
 
 if($cek > 0)
 {
-  session_start();
   $_SESSION['username'] = $username;
   $_SESSION['status'] = "Login";
-
+  foreach ($data as $getData) {
+    $_SESSION['username'] = $getData['username'];
+    $_SESSION['name'] = $getData['name'];
+    $_SESSION['balance'] = $getData['balance'];
+}
   setcookie("mesage", "delete", time()-1); //hapus pesan setcookie
   header("location:index.php");
 }
 else {
-  echo $username.$password;
-//header("location:log.php?pesan=gagal");
+  header("location:index.php?pesan=gagal");
 }
 ?>
